280b3fb186b60b799d5930821b72728b8a16a059
Edvan Oliveira <edvan.oliveiract@gmail.com>
2021-10-30 20:55:18 -0300

    Adicionados repositório, serviço e mapeamento do local

---
+++ src/main/java/br/com/minhavacina/service/LocalService.java
@@ -0,0 +1,43 @@
+package br.com.minhavacina.service;
+
+import br.com.minhavacina.domain.Local;
+import br.com.minhavacina.domain.Municipio;
+import br.com.minhavacina.domain.Usuario;
+import br.com.minhavacina.exception.LancarAdvertencia;
+import br.com.minhavacina.mapper.LocalMapper;
+import br.com.minhavacina.repository.LocalRepository;
+import br.com.minhavacina.request.local.LocalPostRequest;
+import br.com.minhavacina.request.local.LocalPutRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LocalService {
+    private final LocalMapper localMapper;
+    private final LocalRepository localRepository;
+
+    public List<Local> listarLocaisPeloMunicipio(Municipio municipio) {
+        return localRepository.listarLocaisPeloMunicipio(municipio);
+    }
+
+    public Local buscarLocalPorId(Integer id) {
+        return localRepository.findById(id)
+                .orElseThrow(() -> new LancarAdvertencia("Local não encontrado"));
+    }
+
+    public Local cadastrarNovoLocal(LocalPostRequest localPostRequest) {
+        return localRepository.save(localMapper.converterParaLocal(localPostRequest));
+    }
+
+    public void atualizarLocal(LocalPutRequest localPutRequest) {
+        buscarLocalPorId(localPutRequest.getId());
+        localRepository.save(localMapper.converterParaLocal(localPutRequest));
+    }
+
+    public void deletarLocal(Integer id) {
+        localRepository.delete(buscarLocalPorId(id));
+    }
+}
